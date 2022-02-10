4310720eadfc421f4550d9b503c28a80fa011e85
Mizogg <88630056+Mizogg@users.noreply.github.com>
2022-02-10 22:30:10 +0000

    Update HASH160_sequence.py
    HASH160_sequence.py Version 4  need to install # pip install alive_progress     or run new start.bat

--- hash160/HASH160_sequence.py
+++ hash160/HASH160_sequence.py
@@ -1,49 +1,67 @@
 '''
 Made by Mizogg Look for HASH160 Compressed and Uncompressed Using iceland2k14 secp256k1 https://github.com/iceland2k14/secp256k1  fastest Python Libary
 
-Good Luck and Happy Hunting HASH160_sequence.py Version 3 Range division by 1%-1000000% and scan sequentially 
+Good Luck and Happy Hunting HASH160_sequence.py Version 4 Range division by 1%-1000000% and scan sequentially 
 
 https://mizogg.co.uk
 
 '''
 
 import secp256k1 as ice
-import time, multiprocessing
-from multiprocessing import pool, Event, Process, Queue, Value, cpu_count
-from time import sleep
+from alive_progress import alive_bar
+
 
 print('[+] Starting.........Please Wait.....HASH160 List Loading.....')
 
-filename ='puzzleHASH160.txt' #RIPEMD160
+
+filename ='puzzleHASH160.txt' #RIPEMD160  puzzleHASH160
 with open(filename) as f:
     line_count = 0
     for line in f:
         line != "\n"
         line_count += 1
-with open(filename) as file:
-    h160 = file.read().split()
+
+        
+def compute():
+    for i in range(line_count):
+        ...
+        yield
+
+with alive_bar(line_count) as bar:
+    with open(filename) as file:
+        h160 = file.read().split()
+        for i in compute():
+            bar()
+        
+
 h160 = set(h160)
 
-
+        
+       
 print('Total HASH160 Addresses Loaded and Checking : ',str (line_count))
 start=int(input("start range Min 1-115792089237316195423570985008687907852837564279074904382605163141518161494335 ->  "))
 stop=int(input("stop range Max 115792089237316195423570985008687907852837564279074904382605163141518161494336 -> "))
 mag=int(input("Magnitude Jump Stride -> "))
 rangediv=int(input("Division of Range 1% t0 ???% ->  "))
+display =int(input("Choose method Display Method: 1 - BAR:(New Under Testing); 2 - Less Details:(Quicker); 3 - More Details:(Slower)  "))
 print("Starting search... Please Wait min range: " + str(start))
 print("Max range: " + str(stop))
-print("==========================================================")
+print("===========HASH160_sequence.py Version 4 Range division by 1%-1000000% and scan sequentially=============")
 print('Total HASH160 Addresses Loaded and Checking : ',str (line_count))
 
-HEXSTART = "%064x" % start
-HEXSTOP = "%064x" % stop
- 
 remainingtotal=stop-start
 div = round(remainingtotal / rangediv)
 finish = div + start
+finishscan = round(stop / rangediv)
 count = 0
 total = 0
 
+def computermain():
+    for i in range(finishscan):
+        ...
+        yield
+        
+        
 def data_wallet():
     for i in range(0,rangediv):
         percent = div * i
@@ -66,7 +84,6 @@ data = []
 while start < finish:
     try:
         data = []
-        remainingtotal-=mag
         finish-=mag
         start+=mag
         count += 1
@@ -86,9 +103,20 @@ while start < finish:
                     HASH160 Compressed:  {data_w['hash160']}
                     =====Made by mizogg.co.uk Donations 3P7PZLbwSt2bqUMsHF9xDsaNKhafiGuWDB =====""")
         else:
-            for bad_wallet in data:
-                #print('Scan: ', count , ' :Remaining: ', str(finish), ' :Total: ', str(total), end='\r')
-                #print(bad_wallet['percent'], '\nPrivatekey (dec): ', bad_wallet['seed'], '\nPrivatekey (hex): ', bad_wallet['HEX'], '\nHASH160 Uncompressed: ', bad_wallet['hash160u'], '\nHASH160 compressed: ', bad_wallet['hash160'])
-                print(bad_wallet['percent'], '\nPrivatekey (hex): ', bad_wallet['HEX'], end='\r')
+            if display == 1:
+                with alive_bar(finishscan, title='Processing') as bar:
+                    bar(start)
+            elif display == 2:
+                for bad_wallet in data:
+                    print('Scan: ', count , ' :Remaining: ', str(finish), ' :Total: ', str(total), end='\r')
+            elif display == 3:
+                for bad_wallet in data:
+                    #print(bad_wallet['percent'], '\nPrivatekey (dec): ', bad_wallet['seed'], '\nPrivatekey (hex): ', bad_wallet['HEX'], '\nHASH160 Uncompressed: ', bad_wallet['hash160u'], '\nHASH160 compressed: ', bad_wallet['hash160'])
+                    print(bad_wallet['percent'], '\nPrivatekey (hex): ', bad_wallet['HEX'], end='\r')
+            else:
+                print("WRONG NUMBER!!! MUST CHOSE 1, 2 or 3")
+                break
+            
+                    
     except(KeyboardInterrupt, SystemExit):
         exit('\nCTRL-C detected. Exiting gracefully.  Thank you and Happy Hunting')
