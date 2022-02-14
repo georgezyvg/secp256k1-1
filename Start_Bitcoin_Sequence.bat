db3028e60927a30e958baa9aa5fc0e0496cdeb13
Mizogg <88630056+Mizogg@users.noreply.github.com>
2022-02-14 20:48:56 +0000

    Update Start_Bitcoin_Sequence.bat

--- Start_Bitcoin_Sequence.bat
+++ Start_Bitcoin_Sequence.bat
@@ -1,7 +1,7 @@
 @Echo off
-title Bitcoin_sequence.py
+title Bitcoin_sequence_Divison.py
 Pushd "%~dp0"
 pip install alive_progress
 :loop
-python Bitcoin_sequence.py
+python Bitcoin_sequence_Divison.py
 goto loop
